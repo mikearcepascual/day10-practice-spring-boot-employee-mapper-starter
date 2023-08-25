9594ada28749a46c3ab889b7fe4d076544ba19fa
Mikearce Pascual <PASCUMI3@OOCL.COM>
2023-08-25 13:53:26 +0800

    feat: added CompanyResponse class and implemented it on test, controller and service

---
+++ src/main/java/com/afs/restapi/service/dto/CompanyResponse.java
@@ -0,0 +1,35 @@
+package com.afs.restapi.service.dto;
+
+public class CompanyResponse {
+    private Long id;
+    private String name;
+
+    private Integer employeesCount;
+
+    public CompanyResponse() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getEmployeesCount() {
+        return employeesCount;
+    }
+
+    public void setEmployeesCount(Integer employeesCount) {
+        this.employeesCount = employeesCount;
+    }
+}

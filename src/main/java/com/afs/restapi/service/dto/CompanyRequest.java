014d856771c6a281b913efd305435ce9446dac99
Mikearce Pascual <PASCUMI3@OOCL.COM>
2023-08-26 14:36:45 +0800

    refactor: modified test case for should_find_company_by_id and used CompanyRequest and CompanyResponse and added a new constructor in CompanyRequest

--- src/main/java/com/afs/restapi/service/dto/CompanyRequest.java
+++ src/main/java/com/afs/restapi/service/dto/CompanyRequest.java
@@ -1,9 +1,15 @@
 package com.afs.restapi.service.dto;
 
+import com.afs.restapi.entity.Company;
+
 public class CompanyRequest {
     private String name;
 
     public CompanyRequest() {
+    }
+
+    public CompanyRequest(Company company) {
+        this.name = company.getName();
     }
 
     public CompanyRequest(String name) {
